516d8776e4ddec76a421da997ddc4dde5f5ff8c7
akentgo <alexkg007@gmail.com>
2024-06-16 13:27:42 +0200

    Made gitignore and created a small pygame window

---
+++ Battleship/main.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+pygame.init() #we need to initialize the instance
+screen = pygame.display.set_mode((400, 300)) #We make a 400x300 window
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
