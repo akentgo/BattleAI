0b10cfa9e184b4555564a3b86d16052663e74026
akentgo <alexkg007@gmail.com>
2024-06-16 13:30:11 +0200

    Simple command to make the develop branch

--- Battleship/main.py
+++ Battleship/main.py
@@ -5,6 +5,6 @@ pygame.init() #we need to initialize the instance
 screen = pygame.display.set_mode((400, 300)) #We make a 400x300 window
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #Exits with a keypress
             pygame.quit()
             sys.exit()
